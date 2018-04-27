4e38a56e059897b25ba1abb7eea0e0987e011718
hezhuhui <he_zhuhui@163.com>
2018-04-27 16:49:24 +0800

    the ouput is the laste result ,other caffemodel can be ignore.

---
+++ train_squ.sh
@@ -0,0 +1,2 @@
+nohup /home/hzh/caffe/build/tools/caffe train --solver=solver.prototxt -gpu all >&"output_test_lfw_4_27.log"&
+ #/home/hzh/caffe/build/tools/caffe train --solver=solver.prototxt -gpu all 
